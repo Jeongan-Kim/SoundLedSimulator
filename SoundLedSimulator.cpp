ad3efe9d0f41f8aa886f1cc17d3c4e874501bfd1
JeongAn Kim <stor9592@naver.com>
2025-04-07 18:54:12 +0900

    주석 추가

--- SoundLedSimulator.cpp
+++ SoundLedSimulator.cpp
@@ -1,18 +1,20 @@
-﻿#include <iostream>
-#include <portaudio.h>
-#include <cmath>
+﻿#include <iostream>    // 콘솔 출력용 (std::cout)
+#include <portaudio.h> // 마이크 오디오 처리 라이브러리
+#include <cmath>       // abs() 같은 수학 함수
 
-#define SAMPLE_RATE 44100
-#define FRAMES_PER_BUFFER 256
+#define SAMPLE_RATE 44100           // 샘플링 주기 (1초당 44100개 샘플)
+#define FRAMES_PER_BUFFER 256       // 256개 프레임 단위로 콜백 호출됨
 
-// 입력 콜백 함수
-static int audioCallback(const void* inputBuffer, void*, unsigned long framesPerBuffer,
-    const PaStreamCallbackTimeInfo*, PaStreamCallbackFlags, void*) {
-    const float* in = static_cast<const float*>(inputBuffer);
-    float sum = 0;
 
-    for (unsigned long i = 0; i < framesPerBuffer; ++i) {
-        sum += std::abs(in[i]);
+// 입력 콜백 함수(portaudio가 오디오 데이터를 받으면 이 함수를 자동으로 호출함) -> 한번에 256개의 float 샘플이 들어옴
+static int audioCallback(const void* inputBuffer, void*, unsigned long framesPerBuffer, const PaStreamCallbackTimeInfo*, PaStreamCallbackFlags, void*) 
+{
+	const float* in = static_cast<const float*>(inputBuffer); // inputBuffer를 float 포인터로 변환
+    float sum = 0; //평균 볼륨 계산
+
+    for (unsigned long i = 0; i < framesPerBuffer; ++i) 
+    {
+		sum += std::abs(in[i]); // 절대값을 더함 (소리의 크기)
     }
 
     float average = sum / framesPerBuffer;
@@ -22,17 +24,17 @@ static int audioCallback(const void* inputBuffer, void*, unsigned long framesPer
 }
 
 int main() {
-    Pa_Initialize();
+	Pa_Initialize(); // PortAudio 초기화
 
     PaStream* stream;
-    Pa_OpenDefaultStream(&stream, 1, 0, paFloat32, SAMPLE_RATE, FRAMES_PER_BUFFER, audioCallback, nullptr);
-    Pa_StartStream(stream);
+    Pa_OpenDefaultStream(&stream, 1, 0, paFloat32, SAMPLE_RATE, FRAMES_PER_BUFFER, audioCallback, nullptr); // 기본 마이크 입력 열기(콜백함수 등록)
+    Pa_StartStream(stream); // 마이크 입력 시작
 
     std::cout << "▶ Listening... (press Enter to quit)\n";
-    std::cin.get();
+	std::cin.get(); // Enter 키 입력 대기
 
-    Pa_StopStream(stream);
-    Pa_CloseStream(stream);
-    Pa_Terminate();
+	Pa_StopStream(stream); // 마이크 입력 중지
+	Pa_CloseStream(stream); // 스트림 닫기
+	Pa_Terminate(); // PortAudio 종료
     return 0;
 }
